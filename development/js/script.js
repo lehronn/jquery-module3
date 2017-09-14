2c579c2f2aa932a8e334c60eb5629dae9d2e89ad
Mateusz Stomski <lehronn@gmail.com>
2017-09-14 20:32:47 +0200

    it works

--- development/js/script.js
+++ development/js/script.js
@@ -1,47 +1,27 @@
 // scripts.js
-
-//waiting for DOM objects.
-$(function() {
-  console.log('js file is started and DOMs objects is loaded.');
-  //end waiting
-
-  var carouselList = $("#carousel ul");
-
-  var imgs = $("#carousel img");
-  console.log(imgs);
-
-  var kropka = $("body").append('<div class="carousel-counter"></div>');
-  console.log(kropka);
-
-  var countersDiv = $("#carousel-counter div"); console.log(imgs);
-
-  $(imgs).each(function( index, elem ) {
-    $(kropka).append('<i class="fa fa-circle-o" aria-hidden="true">');
-    $('<i class="fa fa-circle-o" aria-hidden="true">').appendTo(".carousel-counter"); //dodaje kropke
-  });
-
-  var changeSlide = function() {
-    setTimeout(changeSlide, 3000);
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+Phone.prototype.printInfo = function() {
+  console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ', and the price is ' + this.price + ' PLN.');
 };
-  var moveFirstSlide = function() {
-    var firstItem = carouselList.find("li:first"),
-        lastItem = carouselList.find("li:last");
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
-  };
-  changeSlide();
 
-  var leftArrow = $( ".fa-arrow-left" ),
-      rightArrow = $( ".fa-arrow-right" );
-  $(leftArrow).click(function() {
-    carouselList.animate({'marginLeft':400}, 500, moveFirstSlide);
-  });
-  $(rightArrow).click(function() {
-  carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-  }
-);
-for (var i = 0; i<= carouselList; i++) {
-  $('<i class="fa fa-circle" aria-hidden="true"></i>');
+Phone.prototype.printSys = function() {
+  console.log('Operating system of ' + this.brand + ' is ' + this.system + '.');
+};
+
+function Phone(brand, price, color, system) {
+  this.brand = brand;
+  this.price = price;
+  this.color = color;
+  this.system = system;
 }
-});
+
+var iPhone6s = new Phone("Apple", '2250', 'silver', 'iOS'),
+    OnePlusOne = new Phone('One Plus', '1450', 'black', 'Android'),
+    SamsungGalaxyS6 = new Phone('Samsung', 2500, 'white', 'Android'),
+    Huawei = new Phone('Huawei', 1100,'gold', 'Android');
+
+iPhone6s.printInfo();
+OnePlusOne.printInfo();
+SamsungGalaxyS6.printInfo();
+
+iPhone6s.printSys();
+Huawei.printSys();
